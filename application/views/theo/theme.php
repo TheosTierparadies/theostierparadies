295e73e92351c68e976e3398d000b955ff27f46c
Joscha Krug <krug@marmalade.de>
2014-03-29 18:41:19 +0100

    Basic version of the theme with changed logo

---
+++ application/views/theo/theme.php
@@ -0,0 +1,12 @@
+<?php
+
+$aTheme = array(
+    'id'            => 'theo',
+    'title'         => 'Theo',
+    'description'   => 'Dies ist das Spezial-Theme für Theos Tierparadies (basiert auf dem Azure-Theme).',
+    'thumbnail'     => 'theme.jpg',
+    'version'       => '0.1',
+    'author'        => 'Theo',
+    'parentTheme'   => 'azure',
+    'parentVersions' => array('1.3')
+);
