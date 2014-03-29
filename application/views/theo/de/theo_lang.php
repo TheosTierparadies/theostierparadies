30bd73da15e0428d56f0f04693322e3c48ca86fe
Joscha Krug <krug@marmalade.de>
2014-03-29 19:19:33 +0100

    Added information about units in stock on the detailspage

---
+++ application/views/theo/de/theo_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$sLangName  = "Deutsch";
+
+$aLang = array(
+    'charset'                       => 'ISO-8859-15',
+    'DETAILS_ARTICLE_STOCK_UNITS'   => 'St&uuml;ck auf Lager:',
+);
---
+++ application/views/theo/en/theo_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$sLangName  = "English";
+
+$aLang = array(
+    'charset'                       => 'ISO-8859-15',
+    'DETAILS_ARTICLE_STOCK_UNITS'   => 'Units in stock:',
+);
